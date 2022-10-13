76a6b2176c02338c6a4e0a8dfcea92c8240c1119
Neil Mastroianni <suancho@gmail.com>
2022-10-13 06:00:25 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.prismic.io', 'images.unsplash.com'],
+  },
+  i18n: {
+    locales: ['en-us'],
+    defaultLocale: 'en-us',
+  },
 }
 
 module.exports = nextConfig
